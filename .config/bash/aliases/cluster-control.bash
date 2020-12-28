3a0140931ebbf5fb32be1641b094c3c1ab19d739
Tomáš Bouška <tomas@buvis.net>
2020-12-28 19:28:08 +0100

    Add update and temperature monitoring commands

--- .config/bash/aliases/cluster-control.bash
+++ .config/bash/aliases/cluster-control.bash
@@ -1,1 +1,3 @@
 alias update-mopidy-hall='kubectl rollout restart deployment mopidy-hall -n mopidy'
+alias get-buvis-temp="ansible-playbook $HOME/git/src/gitlab.com/buvis/playbooks/get-cpu-temperature.yml"
+alias update-buvis-nodes="ansible-playbook $HOME/git/src/gitlab.com/buvis/playbooks/upgrade-arch.yml"
