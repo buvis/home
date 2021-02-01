b24aaf2e731990c32620c89c78d5b34e89c40960
Tomáš Bouška <tomas@buvis.net>
2021-02-01 18:29:35 +0100

    Attempt fixing decryption on other machine

--- .config/bash/variables.bash
+++ .config/bash/variables.bash
@@ -2,3 +2,7 @@ CDPATH=:~/git/src/gitlab.com/doogat
 if [[ $(command -v dircolors) ]]; then
     eval $(dircolors $HOME/.config/dircolors/256dark)
 fi
+
+# Enable transparent gpg encryption handling in vim through vim-gnupg script
+# see: https://github.com/jamessan/vim-gnupg#gpg-agent
+export GPG_TTY=$(tty)
