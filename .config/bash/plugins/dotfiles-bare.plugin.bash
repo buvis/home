99dedd9982ba32bfc49898408d928292fb3a7c6c
Tomáš Bouška <tomas@buvis.net>
2021-02-02 11:40:21 +0100

    Rearrange and hide secrets on changes only

--- .config/bash/plugins/dotfiles-bare.plugin.bash
+++ .config/bash/plugins/dotfiles-bare.plugin.bash
@@ -1,20 +1,15 @@
 cite about-plugin
 about-plugin 'buvis dotfiles management in bare git repo'
 
+alias cfg="/usr/bin/git --git-dir=$HOME/.buvis/ --work-tree=$HOME"
+alias cfga="cfg add"
+alias cfgapa="cfga -p"
+alias cfgm="cfg commit -m"
+alias cfgp="cfg push"
 if [[ $(command -v git-secret) ]]; then
-    alias cfg="/usr/bin/git --git-dir=$HOME/.buvis/ --work-tree=$HOME"
-    alias cfga="cfg secret hide;cfg add"
-    alias cfgapa="cfg secret hide;cfga -p"
-    alias cfgm="cfg commit -m"
-    alias cfgp="cfg push"
-    alias cfgl="cfg pull && cfg submodule update --init && cfg submodule update --remote --merge;cfg secret reveal"
-    alias cfgs="cfg secret hide;cfg status"
+    alias cfgl="cfg pull && cfg submodule update --init && cfg submodule update --remote --merge; cfg secret reveal"
+    alias cfgs="cfg secret hide -m; cfg status"
 else
-    alias cfg="/usr/bin/git --git-dir=$HOME/.buvis/ --work-tree=$HOME"
-    alias cfga="cfg add"
-    alias cfgapa="cfga -p"
-    alias cfgm="cfg commit -m"
-    alias cfgp="cfg push"
     alias cfgl="cfg pull && cfg submodule update --init && cfg submodule update --remote --merge"
     alias cfgs="cfg status"
 fi
