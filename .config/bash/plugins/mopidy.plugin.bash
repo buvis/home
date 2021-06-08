d563c9ae80d7c897caeb9823dd4acf32476abcb8
Tomáš Bouška <tomas@buvis.net>
2021-06-08 13:31:13 +0200

    Add support for linux/amd64

--- .config/bash/plugins/mopidy.plugin.bash
+++ .config/bash/plugins/mopidy.plugin.bash
@@ -5,14 +5,19 @@ export BUVIS_AWS_ACCOUNT=473900256917
 export BUVIS_AWS_REGION=eu-west-1
 export BUVIS_AWS_ECR=${BUVIS_AWS_ACCOUNT}.dkr.ecr.${BUVIS_AWS_REGION}.amazonaws.com
 export BUVIS_MOPIDY_IMAGE=${BUVIS_AWS_ECR}/mopidy
-export BUVIS_MOPIDY_PLATFORM=linux/arm/v6
+export BUVIS_MOPIDY_PLATFORM=linux/arm/v6,linux/amd64
 
 # build mopidy image to buvis AWS ECR
 function build-mopidy () {
     aws ecr get-login-password --region ${BUVIS_AWS_REGION} | docker login --username AWS --password-stdin ${BUVIS_AWS_ECR}
+    docker buildx use multiarch # use a newer builder to support multi-arch images. If builder doesn't exist, create it: docker buildx create --name multiarch
     docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
-    docker buildx build --no-cache --platform ${BUVIS_MOPIDY_PLATFORM} -t ${BUVIS_MOPIDY_IMAGE} ~/.dockerfiles/mopidy
-    docker push ${BUVIS_MOPIDY_IMAGE}
+    docker buildx build \
+        --no-cache \
+        --push \
+        --platform ${BUVIS_MOPIDY_PLATFORM} \
+        --tag ${BUVIS_MOPIDY_IMAGE} \
+        ~/.dockerfiles/mopidy
 }
 
 # print container log from remote machine
