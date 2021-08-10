8987d6416d8e06b16317901b7f3b3498742e648a
Tomáš Bouška <tomas@buvis.net>
2021-08-10 10:44:11 +0200

    Remove obsolete aliases

--- .config/bash/aliases/cluster-control.bash
+++
@@ -1,3 +0,0 @@
-alias update-mopidy-hall='kubectl rollout restart deployment mopidy-hall -n mopidy'
-alias get-buvis-temp="ansible-playbook $HOME/git/src/gitlab.com/buvis/playbooks/get-cpu-temperature.yml"
-alias update-buvis-nodes="ansible-playbook $HOME/git/src/gitlab.com/buvis/playbooks/upgrade-arch.yml"
