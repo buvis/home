fb431cc5d2f1498174b27dd3d841457c3324ed10
Tomáš Bouška <tomas@buvis.net>
2021-09-02 11:54:24 +0200

    Fix typo in path

--- bin/clean-junk.py
+++ bin/clean-junk.py
@@ -13,9 +13,9 @@ def clean_folder(path):
     for filename in os.listdir(path):
         os.remove(os.path.join(path, filename))
 
-clean_folder(r"C:\Users\Desktop")
 clean_folder(r"C:\Users\Public\Desktop")
 clean_folder(r"C:\Users\tbouska\OneDrive - Automatic Data Processing Inc\Desktop")
+clean_folder(r"C:\Users\tbouska\Desktop")
 
 # Unwanted startup items
 startup_folder = r"C:\Users\tbouska\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
