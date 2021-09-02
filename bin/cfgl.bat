6118d460df6fc61f4cf0276d956090e53a02d04f
Tomáš Bouška <tomas@buvis.net>
2021-09-02 10:52:57 +0200

    Add buvis repo management batch scripts

---
+++ bin/cfgl.bat
@@ -0,0 +1,3 @@
+git --git-dir=C:\Users\tbouska\.buvis --work-tree=C:\Users\tbouska pull
+git --git-dir=C:\Users\tbouska\.buvis --work-tree=C:\Users\tbouska submodule update --init --recursive
+git --git-dir=C:\Users\tbouska\.buvis --work-tree=C:\Users\tbouska submodule update --remote --merge
---
+++ bin/cfgs.bat
@@ -0,0 +1,1 @@
+git --git-dir=C:\Users\tbouska\.buvis --work-tree=C:\Users\tbouska status
