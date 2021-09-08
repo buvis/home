e21422cf6d9ac74c66bbd1a828e13aa04e9482ab
Tomáš Bouška <tomas@buvis.net>
2021-09-08 10:34:17 +0200

    Remove irrelevant piece of code

--- bin/clean-junk.py
+++ bin/clean-junk.py
@@ -1,13 +1,5 @@
-import pyautogui
 import os
-from datetime import datetime
 
-
-dt_now = datetime.now()
-minute = dt_now.minute
-if minute % 4 == 0:
-    pyautogui.press('numlock')
-    pyautogui.press('numlock')
 
 def clean_folder(path):
     for filename in os.listdir(path):
@@ -20,7 +12,9 @@ clean_folder(r"C:\Users\tbouska\Desktop")
 # Unwanted startup items
 startup_folder = r"C:\Users\tbouska\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
 unwanted_files = ["Map.exe", "EUCnwvpn.exe", "My ADP Portal.url"]
+
 for filename in unwanted_files:
     full_path = os.path.join(startup_folder, filename)
+
     if os.path.isfile(full_path):
         os.remove(full_path)
