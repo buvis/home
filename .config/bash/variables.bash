8083536d73a5636c14a873cdd400341690f7cce2
Tomáš Bouška <tomas@buvis.net>
2021-11-11 12:25:01 +0100

    Fix HOME reference in WSL

--- .config/bash/variables.bash
+++ .config/bash/variables.bash
@@ -1,4 +1,4 @@
 CDPATH=:~/git/src/gitlab.com/doogat
 if [[ $(command -v dircolors) ]]; then
-    eval $(dircolors $HOME/.config/dircolors/256dark)
+    eval $(dircolors $DOTFILES_ROOT/.config/dircolors/256dark)
 fi
