0705eed611c5055bfbc467dca182110a5ce95a01
Tomáš Bouška <tomas@buvis.net>
2021-12-07 17:49:43 +0100

    Fix variables

--- get-buvis.bat
+++ get-buvis.bat
@@ -1,13 +1,13 @@
 Rem Remove any previous installations
 cd %HOME%
-set REPO=$HOME/.buvis
-rmdir /s %REPO%
+set REPO=%HOME%\.buvis
+rmdir /Q /s %REPO%
 
 Rem Clone as bare repository
 git clone --bare https://github.com/tbouska/buvis.git %REPO%
 
 Rem Don't list untracked files in git status
-git --git-dir=%REPO% --work-tree=$HOME config status.showUntrackedFiles no
+git --git-dir=%REPO% --work-tree=%HOME% config status.showUntrackedFiles no
 
 Rem Avoid conflicts on checkout
 echo Remove submodules listed in .gitmodules
@@ -16,18 +16,20 @@ echo Backup and remove conflicting files/folders (see repository at https://gith
 pause > nul
 
 Rem Checkout
-git --git-dir=%REPO% --work-tree=$HOME checkout
+git --git-dir=%REPO% --work-tree=%HOME% checkout
 
 Rem Retrieve submodules' content
-git --git-dir=%REPO% --work-tree=$HOME submodule update --init
-git --git-dir=%REPO% --work-tree=$HOME submodule update --remote --merge
+git --git-dir=%REPO% --work-tree=%HOME% submodule update --init
+git --git-dir=%REPO% --work-tree=%HOME% submodule update --remote --merge
 
 Rem Track master for pull/push
-git --git-dir=%REPO% --work-tree=$HOME push -u origin master
+git --git-dir=%REPO% --work-tree=%HOME% push -u origin master
 
 Rem Keep README.md and get-buvis.sh in remote only (it is meant for public, and I don't need it on my machines)
-del $HOME/README.md 
-del $HOME/get-buvis.sh
-del $HOME/get-buvis.bat
-git --git-dir=%REPO% --work-tree=$HOME update-index --skip-worktree README.md get-buvis.sh get-buvis.bat
+del %HOME%\README.md 
+del %HOME%\get-buvis.sh
+del %HOME%\get-buvis.bat
+git --git-dir=%REPO% --work-tree=%HOME% update-index --skip-worktree README.md get-buvis.sh get-buvis.bat
 
+Rem Create symlinks
+mklink /d %APPDATA%\alacritty %HOME%\.config\alacritty
