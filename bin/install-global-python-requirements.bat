0535069cca168b8e2c7ce8d119fd97acc5b89195
Tomáš Bouška <tomas.bouska@adp.com>
2021-12-08 15:24:39 +0100

    Update

--- bin/install-global-python-requirements.bat
+++ bin/install-global-python-requirements.bat
@@ -1,1 +1,1 @@
-pip install unicode-slugify PyAutoGUI pipenv pyreadline flask_reverse_proxy_fix Werkzeug==0.16.1 flask flask_admin flask_api flask_bootstrap flask_paginate flask_wtf arrow buku
+pip install unicode-slugify PyAutoGUI pipenv pyreadline
