faf9b4dd620b494a1fea75336fbfe46566e6d028
Tomáš Bouška <tomas@buvis.net>
2022-03-29 16:37:16 +0200

    Fix mopidy runner to work with Arch Linux host

--- .config/bash/plugins/mopidy.plugin.bash
+++ .config/bash/plugins/mopidy.plugin.bash
@@ -31,9 +31,12 @@ function run-mopidy () {
     ssh $1 "docker pull buvis/mopidy && docker run --detach --restart=always \
                 -p 6680:6680 -p 6600:6600 \
                 --device /dev/snd \
+                --group-add \$(getent group audio | cut -d: -f3) \
                 --mount type=bind,source=/var/local/docker/mopidy/config,target=/app/config,readonly \
                 --mount type=bind,source=/var/local/docker/mopidy/media,target=/var/lib/mopidy/media,readonly \
                 --mount type=bind,source=/var/local/docker/mopidy/local,target=/var/lib/mopidy/local \
                 --mount type=bind,source=/var/local/docker/mopidy/playlists,target=/var/lib/mopidy/playlists \
                 --name mopidy buvis/mopidy"
+    echo "Don't forget to authenticate to Tidal in the next 5 minutes.\n"
+    echo "Get the OAuth link from the logs: `ssh $1 docker logs mopidy`"
 }
