2dae24be218a4b72196eb191abeb6f73abc123dc
Tomáš Bouška <tomas@buvis.net>
2022-06-23 19:37:03 +0200

    Update mopidy functions for newer image version

--- .config/bash/plugins/mopidy.plugin.bash
+++ .config/bash/plugins/mopidy.plugin.bash
@@ -15,11 +15,12 @@ function update-mopidy () {
             #;;
             ## other players aren't part of cluster (for now)
         *)
-            # Archive current image
-            ssh $1 "docker stop mopidy;\
-            docker stop mopidy-prev;\
-            docker rm -v mopidy-prev;\
-            docker rename mopidy mopidy-prev"
+            # Get new and archive the previous container
+            ssh $1 "docker pull buvis/mopidy; \
+            docker stop mopidy >/dev/null 2>&1; \
+            docker stop mopidy-prev >/dev/null 2>&1; \
+            docker rm -v mopidy-prev >/dev/null 2>&1; \
+            docker rename mopidy mopidy-prev >/dev/null 2>&1"
             # Run mopidy with new image
             run-mopidy $1
             ;;
@@ -30,13 +31,19 @@ function update-mopidy () {
 function run-mopidy () {
     ssh $1 "docker pull buvis/mopidy && docker run --detach --restart=always \
                 -p 6680:6680 -p 6600:6600 \
+                -u kah:audio \
                 --device /dev/snd \
                 --group-add \$(getent group audio | cut -d: -f3) \
-                --mount type=bind,source=/var/local/docker/mopidy/config,target=/app/config,readonly \
-                --mount type=bind,source=/var/local/docker/mopidy/media,target=/var/lib/mopidy/media,readonly \
-                --mount type=bind,source=/var/local/docker/mopidy/local,target=/var/lib/mopidy/local \
-                --mount type=bind,source=/var/local/docker/mopidy/playlists,target=/var/lib/mopidy/playlists \
+                --mount type=bind,source=/var/local/docker/mopidy/config,target=/config \
+                --mount type=bind,source=/var/local/docker/mopidy/media,target=/app/mopidy/media,readonly \
+                --mount type=bind,source=/var/local/docker/mopidy/local,target=/app/mopidy/local \
+                --mount type=bind,source=/var/local/docker/mopidy/playlists,target=/app/mopidy/playlists \
                 --name mopidy buvis/mopidy"
     echo "Don't forget to authenticate to Tidal in the next 5 minutes.\n"
-    echo "Get the OAuth link from the logs: `ssh $1 docker logs mopidy`"
+    echo "Get the OAuth link from the logs: ssh $1 docker logs mopidy"
+}
+
+# scan local library
+function scan-mopidy () {
+    ssh $1 "docker exec mopidy /shim/scan-local.sh"
 }
