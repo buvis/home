0c9c15f7160c3e5a50d27bb9620f33be68ba59ea
Tomáš Bouška <tomas@buvis.net>
2022-07-29 13:54:15 +0200

    Fix vpn credentials backup

--- .config/bash/plugins/backup.plugin.bash
+++ .config/bash/plugins/backup.plugin.bash
@@ -7,10 +7,10 @@ function backup-router () {
     cd $DOTFILES_ROOT/Downloads/router-backup
     mkdir -p etc
     mkdir -p etc/mail
+    mkdir -p etc/protonvpn
     mkdir -p etc/ssh
     mkdir -p home/bob
     mkdir -p var/unbound/etc
-    mkdir -p var/protonvpn/etc
     scp orcus:/etc/bgpd.conf etc/bgpd.conf
     scp orcus:/etc/dhclient.conf etc/dhclient.conf
     scp orcus:/etc/dhcpd.conf etc/dhcpd.conf
@@ -19,16 +19,17 @@ function backup-router () {
     scp orcus:/etc/hostname.em0 etc/hostname.em0
     scp orcus:/etc/hostname.em1 etc/hostname.em1
     scp orcus:/etc/hostname.em2 etc/hostname.em2
+    scp orcus:/etc/hostname.bridge0 etc/hostname.bridge0
     scp orcus:/etc/hosts etc/hosts
     scp orcus:/etc/mail/smtpd.conf etc/mail/smtpd.conf
     scp orcus:/etc/newsyslog.conf etc/newsyslog.conf
     scp orcus:/etc/pf.conf etc/pf.conf
+    scp -r orcus:/etc/protonvpn/ etc/protonvpn/
     scp orcus:/etc/rc.conf.local etc/rc.conf.local
     scp orcus:/etc/resolv.conf.tail etc/resolv.conf.tail
     scp orcus:/etc/ssh/sshd_config etc/ssh/sshd_config
     scp orcus:/etc/sysctl.conf etc/ssh/sysctl.conf
     scp orcus:/home/bob/send-vim-tip.sh home/bob/send-vim-tip.sh
     scp -r orcus:/var/unbound/etc/ var/unbound/
-    scp -r orcus:/var/protonvpn/etc/ var/protonvpn/
     cd -
 }
