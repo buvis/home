a452a8142b156512aaa2e3e40d92b552b0d5439f
Tomáš Bouška <tomas@buvis.net>
2023-01-05 21:34:36 +0100

    Add Reader to web apps

--- .webapps/feedly/build.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-# Build Feedly app using nativefier (https://github.com/nativefier/nativefier)
-nativefier --name "Feedly" --internal-urls ".*?" --icon feedly.icns "http://feedly.com/" ~/Applications/webapps
