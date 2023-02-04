f0efce74111af3119dc7101bed09e17e447900ac
Tomáš Bouška [WSL] <tomas@buvis.net>
2023-02-04 14:33:23 +0100

    Add Windows config management scripts

---
+++ bin/cfgl.bat
@@ -0,0 +1,3 @@
+git --git-dir=C:\Users\tbouska\.buvis --work-tree=C:\Users\tbouska pull
+git --git-dir=C:\Users\tbouska\.buvis --work-tree=C:\Users\tbouska submodule update --init
+git --git-dir=C:\Users\tbouska\.buvis --work-tree=C:\Users\tbouska submodule update --remote --merge
