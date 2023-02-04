e852891fe52e455030a92cfc0eeb348630a8ff6b
Tomáš Bouška [WSL] <tomas@buvis.net>
2023-02-04 08:42:01 +0100

    Add WSL updater, remove VPN fixer

--- bin/fixAnyConnectMetric.ps1
+++
@@ -1,2 +0,0 @@
-Get-NetIPInterface -InterfaceAlias "vEthernet (WSL)" | Set-NetIPInterface -InterfaceMetric 1
-Get-NetAdapter | Where-Object {$_.InterfaceDescription -Match "Cisco AnyConnect"} | Set-NetIPInterface -InterfaceMetric 6000
