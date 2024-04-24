fceefb15a3b68449c1dc53fbd6386ce4408356a3
Tomáš Bouška <tomas@buvis.net>
2024-04-24 19:31:06 +0200

    Remove unused functions

--- .config/bash/plugins/zetbrain.plugin.bash
+++
@@ -1,9 +0,0 @@
-cite about-plugin
-about-plugin 'zetbrain tools'
-
-# synchronize
-function zsync () {
-    cd $HOME/z/reference/notes
-    git pull && git add . && git commit -m "sync" && git push
-    cd -
-}
