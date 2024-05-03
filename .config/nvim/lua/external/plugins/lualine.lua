2b085799cedd72bd550f835462bf8f51dddd8262
Tomáš Bouška <tomas@buvis.net>
2024-05-04 00:27:32 +0200

    Show path relative to project's root

--- .config/nvim/lua/external/plugins/lualine.lua
+++ .config/nvim/lua/external/plugins/lualine.lua
@@ -2,6 +2,15 @@ local custom_solarized = require('lualine.themes.solarized_dark')
 
 custom_solarized.normal.c.bg = '#002b36'
 
-require('lualine').setup {
+require('lualine').setup({
   options = { theme = custom_solarized },
-}
+  sections = {
+    lualine_c = {
+      {
+        'filename',
+        file_status = true, -- displays file status (readonly status, modified status)
+        path = 1 -- display path relative to project's root
+      }
+    }
+  },
+})
