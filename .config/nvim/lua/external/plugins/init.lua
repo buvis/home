f3a1aba27325f26317d48460b520930c94c8956f
Tomáš Bouška <tomas@buvis.net>
2024-05-03 20:39:04 +0200

    Move lazy loaded plugins in their own package

--- .config/nvim/lua/external/plugins/init.lua
+++ .config/nvim/lua/external/plugins/init.lua
@@ -19,21 +19,8 @@ if vim.o.loadplugins then
   -- https://github.com/tpope/vim-commentary
   buvis.plugin.load('vim-commentary')
 
-  -- Lazy because it adds a slow BufEnter autocmd.
-  buvis.plugin.lazy('nvim-tree.lua', {
-    afterload = function()
-      require('nvim-tree').setup()
-    end,
-    commands = {
-      'NvimTreeFindFile',
-      'NvimTreeToggle',
-      'NvimTreeOpen',
-   },
-    keymap = {
-      { 'n', '<LocalLeader>f', ':NvimTreeFindFile<CR>', { silent = true } },
-      { 'n', '<LocalLeader>t', ':NvimTreeToggle<CR>', { silent = true } },
-    },
-  })
+  -- Lazy loaded plugins (because they slow down the startup)
+  require('external.plugins.lazy')
 end
 
 -- Automatic, language-dependent indentation, syntax coloring and other
@@ -43,4 +30,3 @@ end
 -- package "ftdetect" directories won't be evaluated.
 vim.cmd('filetype indent plugin on')
 vim.cmd('syntax on')
-
