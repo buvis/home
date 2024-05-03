6dd8d355d097d82982701cfab4ebd1ba1ed09d72
Tomáš Bouška <tomas@buvis.net>
2024-05-04 00:50:11 +0200

    Search inside files

--- .config/nvim/lua/external/plugins/telescope.lua
+++ .config/nvim/lua/external/plugins/telescope.lua
@@ -7,7 +7,7 @@ require('telescope').setup({
         ["<C-j>"] = require("telescope.actions").move_selection_next,
       }
     }
-  }
+  },
 })
 
 require("telescope").load_extension("zf-native")
@@ -15,3 +15,4 @@ require("telescope").load_extension("zf-native")
 local keymap = vim.keymap
 
 keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<CR>", { desc = "Fuzzy find files in cwd" })
+keymap.set("n", "<leader>fg", "<cmd>Telescope live_grep<CR>", { desc = "Fuzzy find in files' content in cwd" })
