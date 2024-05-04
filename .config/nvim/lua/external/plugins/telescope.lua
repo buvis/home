9d8ffd9aa3e1412b614f88279fcba3efbd7a53ed
Tomáš Bouška <tomas@buvis.net>
2024-05-04 09:36:15 +0200

    Add keymaps for Telescope

--- .config/nvim/lua/external/plugins/telescope.lua
+++ .config/nvim/lua/external/plugins/telescope.lua
@@ -15,4 +15,6 @@ require("telescope").load_extension("zf-native")
 local keymap = vim.keymap
 
 keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<CR>", { desc = "Fuzzy find files in cwd" })
-keymap.set("n", "<leader>fg", "<cmd>Telescope live_grep<CR>", { desc = "Fuzzy find in files' content in cwd" })
+keymap.set("n", "<leader>fr", "<cmd>Telescope old_files<CR>", { desc = "Fuzzy find recent files" })
+keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<CR>", { desc = "Find string in cwd" })
+keymap.set("n", "<leader>f*", "<cmd>Telescope grep_string<CR>", { desc = "Find string under cursor in cwd" })
