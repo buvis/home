90abe16e46313f1387b1fa1943bea361663d671a
Tomáš Bouška <tomas@buvis.net>
2024-05-05 08:39:41 +0200

    Disable plugin config change notification

--- .config/nvim/lua/buvis/lazy.lua
+++ .config/nvim/lua/buvis/lazy.lua
@@ -11,4 +11,6 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("buvis.plugins")
+require("lazy").setup("buvis.plugins", {
+  change_detection = { notify = false, },
+})
