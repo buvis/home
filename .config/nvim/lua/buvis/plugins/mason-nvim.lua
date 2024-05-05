5e3797b611ea2eccb96d70cc489fdabd3ce5154a
Tomáš Bouška <tomas@buvis.net>
2024-05-05 12:40:50 +0200

    Switch to ruff

--- .config/nvim/lua/buvis/plugins/mason-nvim.lua
+++ .config/nvim/lua/buvis/plugins/mason-nvim.lua
@@ -1,49 +1,47 @@
 return {
-	"williamboman/mason.nvim",
-	dependencies = {
-		"williamboman/mason-lspconfig.nvim",
-		"WhoIsSethDaniel/mason-tool-installer.nvim",
-	},
-	config = function()
-		local mason = require("mason")
-		local mason_lspconfig = require("mason-lspconfig")
-		local mason_tool_installer = require("mason-tool-installer")
+  "williamboman/mason.nvim",
+  dependencies = {
+    "williamboman/mason-lspconfig.nvim",
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+  },
+  config = function()
+    local mason = require("mason")
+    local mason_lspconfig = require("mason-lspconfig")
+    local mason_tool_installer = require("mason-tool-installer")
 
-		mason.setup({
-			ui = {
-				icons = {
-					package_installed = "✓",
-					package_pending = "➜",
-					package_uninstalled = "✗",
-				},
-			},
-		})
+    mason.setup({
+      ui = {
+        icons = {
+          package_installed = "✓",
+          package_pending = "➜",
+          package_uninstalled = "✗",
+        },
+      },
+    })
 
-		mason_lspconfig.setup({
-			-- list of servers for mason to install
-			ensure_installed = {
-				"cssls",
-				"emmet_ls",
-				"graphql",
-				"html",
-				"lua_ls",
-				"pyright",
-				"ruff",
-				"svelte",
-				"tailwindcss",
-				"tsserver",
-			},
-		})
+    mason_lspconfig.setup({
+      -- list of language servers for Mason to install
+      ensure_installed = {
+        "cssls",
+        "emmet_ls",
+        "graphql",
+        "html",
+        "lua_ls",
+        "pyright",
+        "ruff_lsp",
+        "svelte",
+        "tailwindcss",
+        "tsserver",
+      },
+    })
 
-		mason_tool_installer.setup({
-			ensure_installed = {
-				"prettier",
-				"stylua",
-				"isort",
-				"black",
-				"pylint",
-				"eslint_d",
-			},
-		})
-	end,
+    mason_tool_installer.setup({
+      -- list of linters and formatters for Mason to install
+      ensure_installed = {
+        "eslint_d",
+        "prettier",
+        "stylua",
+      },
+    })
+  end,
 }
