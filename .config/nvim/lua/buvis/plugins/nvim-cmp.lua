c1a76cd24aad3b8b6825670a6c89890bdc01f8e7
Tomáš Bouška <tomas.bouska@adp.com>
2024-05-09 10:28:07 +0200

    Fix space not typing in some cases

--- .config/nvim/lua/buvis/plugins/nvim-cmp.lua
+++ .config/nvim/lua/buvis/plugins/nvim-cmp.lua
@@ -42,7 +42,7 @@ return {
         ["<Tab>"] = cmp.mapping.select_next_item(), -- next suggestion
         ["<C-b>"] = cmp.mapping.scroll_docs(-4),
         ["<C-f>"] = cmp.mapping.scroll_docs(4),
-        ["<leader><leader>"] = cmp.mapping.complete(), -- show completion suggestions
+        ["S-<leader>"] = cmp.mapping.complete(), -- show completion suggestions
         ["<Esc>"] = cmp.mapping.abort(), -- close completion window
         ["<CR>"] = cmp.mapping.confirm({ select = false }),
       }),
