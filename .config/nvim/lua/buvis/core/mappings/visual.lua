490c3cb4caa8bf54e44470d60d9ea419c17be2ba
Tomáš Bouška <tomas@buvis.net>
2024-05-21 22:01:14 +0200

    Add shortcut to yank to clipboard

--- .config/nvim/lua/buvis/core/mappings/visual.lua
+++ .config/nvim/lua/buvis/core/mappings/visual.lua
@@ -1,3 +1,6 @@
 -- Replace highlighted text
 -- https://stackoverflow.com/a/676619
 vim.cmd('vnoremap <C-r> "hy:%s/<C-r>h//gc<left><left><left>')
+
+-- Copy selection to system clipboard
+vim.cmd('vnoremap Y "*y')
