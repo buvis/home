7654e2cd88b6b39e38463f5f840eb396ca7eeedf
Tomáš Bouška <tomas@buvis.net>
2024-05-28 17:02:09 +0200

    Fix copy to clipboard

--- .config/nvim/lua/buvis/core/mappings/visual.lua
+++ .config/nvim/lua/buvis/core/mappings/visual.lua
@@ -1,6 +1,8 @@
+local keymap = vim.keymap
+
 -- Replace highlighted text
 -- https://stackoverflow.com/a/676619
 vim.cmd('vnoremap <C-r> "hy:%s/<C-r>h//gc<left><left><left>')
 
 -- Copy selection to system clipboard
-vim.cmd('vnoremap Y "*y')
+keymap.set("v", "Y", [["*y]], { desc = "Yank to system clipboard" })
