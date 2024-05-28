d0fe7dbae122110d9899c50699f61a5c05526e43
Tomáš Bouška <tomas@buvis.net>
2024-05-28 16:28:57 +0200

    Increase visibility of inactive panes

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -3,36 +3,36 @@ local act = wezterm.action
 
 -- integration with Neovim
 local function is_vim(pane)
-  -- this is set by the plugin, and unset on ExitPre in Neovim
-  return pane:get_user_vars().IS_NVIM == 'true'
+	-- this is set by the plugin, and unset on ExitPre in Neovim
+	return pane:get_user_vars().IS_NVIM == "true"
 end
 
 local direction_keys = {
-  h = 'Left',
-  j = 'Down',
-  k = 'Up',
-  l = 'Right',
+	h = "Left",
+	j = "Down",
+	k = "Up",
+	l = "Right",
 }
 
 local function split_nav(resize_or_move, key)
-  return {
-    key = key,
-    mods = resize_or_move == 'resize' and 'META' or 'CTRL',
-    action = wezterm.action_callback(function(win, pane)
-      if is_vim(pane) then
-        -- pass the keys through to vim/nvim
-        win:perform_action({
-          SendKey = { key = key, mods = resize_or_move == 'resize' and 'META' or 'CTRL' },
-        }, pane)
-      else
-        if resize_or_move == 'resize' then
-          win:perform_action({ AdjustPaneSize = { direction_keys[key], 3 } }, pane)
-        else
-          win:perform_action({ ActivatePaneDirection = direction_keys[key] }, pane)
-        end
-      end
-    end),
-  }
+	return {
+		key = key,
+		mods = resize_or_move == "resize" and "META" or "CTRL",
+		action = wezterm.action_callback(function(win, pane)
+			if is_vim(pane) then
+				-- pass the keys through to vim/nvim
+				win:perform_action({
+					SendKey = { key = key, mods = resize_or_move == "resize" and "META" or "CTRL" },
+				}, pane)
+			else
+				if resize_or_move == "resize" then
+					win:perform_action({ AdjustPaneSize = { direction_keys[key], 3 } }, pane)
+				else
+					win:perform_action({ ActivatePaneDirection = direction_keys[key] }, pane)
+				end
+			end
+		end),
+	}
 end
 
 local config = {}
@@ -41,62 +41,62 @@ config.audible_bell = "Disabled"
 config.check_for_updates = false
 config.color_scheme = "Builtin Solarized Dark"
 config.inactive_pane_hsb = {
-  hue = 0.22,
-  saturation = 0.44,
-  brightness = 0.55,
+	hue = 0.15,
+	saturation = 0.65,
+	brightness = 0.75,
 }
-config.font = wezterm.font 'MesloLGS NF'
+config.font = wezterm.font("MesloLGS NF")
 
-if wezterm.target_triple == 'x86_64-pc-windows-msvc' then
-  config.font_size = 9.5
+if wezterm.target_triple == "x86_64-pc-windows-msvc" then
+	config.font_size = 9.5
 else
-  config.font_size = 11
+	config.font_size = 11
 end
 
 config.launch_menu = {}
-config.leader = { key="q", mods = "CTRL" }
+config.leader = { key = "q", mods = "CTRL" }
 config.disable_default_key_bindings = true
 config.keys = {
-  { key = "1", mods = "LEADER",       action=act{ActivateTab=0}},
-  { key = "2", mods = "LEADER",       action=act{ActivateTab=1}},
-  { key = "3", mods = "LEADER",       action=act{ActivateTab=2}},
-  { key = "4", mods = "LEADER",       action=act{ActivateTab=3}},
-  { key = "5", mods = "LEADER",       action=act{ActivateTab=4}},
-  { key = "6", mods = "LEADER",       action=act{ActivateTab=5}},
-  { key = "7", mods = "LEADER",       action=act{ActivateTab=6}},
-  { key = "8", mods = "LEADER",       action=act{ActivateTab=7}},
-  { key = "9", mods = "LEADER",       action=act{ActivateTab=8}},
-  { key = "c", mods = "LEADER",       action=act{SpawnTab="CurrentPaneDomain"}},
-  { key = "h", mods = "LEADER",       action=act{ActivatePaneDirection="Left"}},
-  { key = "j", mods = "LEADER",       action=act{ActivatePaneDirection="Down"}},
-  { key = "k", mods = "LEADER",       action=act{ActivatePaneDirection="Up"}},
-  { key = "l", mods = "LEADER",       action=act{ActivatePaneDirection="Right"}},
-  { key = "s", mods = "LEADER",       action=act{SplitVertical={domain="CurrentPaneDomain"}}},
-  { key = "v", mods = "LEADER",       action=act{SplitHorizontal={domain="CurrentPaneDomain"}}},
-  { key = "w", mods = "LEADER",       action=act{CloseCurrentTab={confirm=true}}},
-  { key = "q", mods = "LEADER",       action=act{CloseCurrentPane={confirm=true}}},
-  { key = "z", mods = "LEADER",       action="TogglePaneZoomState" },
-  { key = "c", mods = "CTRL",         action=act.CopyTo 'Clipboard'},
-  { key = "v", mods = "CTRL",         action=act.PasteFrom 'Clipboard'},
-  { key = "c", mods = "CTRL|SHIFT",   action=act.SendKey{key = 'c', mods = 'CTRL'}},
-  -- move between split panes
-  split_nav('move', 'h'),
-  split_nav('move', 'j'),
-  split_nav('move', 'k'),
-  split_nav('move', 'l'),
-  -- resize panes
-  split_nav('resize', 'h'),
-  split_nav('resize', 'j'),
-  split_nav('resize', 'k'),
-  split_nav('resize', 'l'),
+	{ key = "1", mods = "LEADER", action = act({ ActivateTab = 0 }) },
+	{ key = "2", mods = "LEADER", action = act({ ActivateTab = 1 }) },
+	{ key = "3", mods = "LEADER", action = act({ ActivateTab = 2 }) },
+	{ key = "4", mods = "LEADER", action = act({ ActivateTab = 3 }) },
+	{ key = "5", mods = "LEADER", action = act({ ActivateTab = 4 }) },
+	{ key = "6", mods = "LEADER", action = act({ ActivateTab = 5 }) },
+	{ key = "7", mods = "LEADER", action = act({ ActivateTab = 6 }) },
+	{ key = "8", mods = "LEADER", action = act({ ActivateTab = 7 }) },
+	{ key = "9", mods = "LEADER", action = act({ ActivateTab = 8 }) },
+	{ key = "c", mods = "LEADER", action = act({ SpawnTab = "CurrentPaneDomain" }) },
+	{ key = "h", mods = "LEADER", action = act({ ActivatePaneDirection = "Left" }) },
+	{ key = "j", mods = "LEADER", action = act({ ActivatePaneDirection = "Down" }) },
+	{ key = "k", mods = "LEADER", action = act({ ActivatePaneDirection = "Up" }) },
+	{ key = "l", mods = "LEADER", action = act({ ActivatePaneDirection = "Right" }) },
+	{ key = "s", mods = "LEADER", action = act({ SplitVertical = { domain = "CurrentPaneDomain" } }) },
+	{ key = "v", mods = "LEADER", action = act({ SplitHorizontal = { domain = "CurrentPaneDomain" } }) },
+	{ key = "w", mods = "LEADER", action = act({ CloseCurrentTab = { confirm = true } }) },
+	{ key = "q", mods = "LEADER", action = act({ CloseCurrentPane = { confirm = true } }) },
+	{ key = "z", mods = "LEADER", action = "TogglePaneZoomState" },
+	{ key = "c", mods = "CTRL", action = act.CopyTo("Clipboard") },
+	{ key = "v", mods = "CTRL", action = act.PasteFrom("Clipboard") },
+	{ key = "c", mods = "CTRL|SHIFT", action = act.SendKey({ key = "c", mods = "CTRL" }) },
+	-- move between split panes
+	split_nav("move", "h"),
+	split_nav("move", "j"),
+	split_nav("move", "k"),
+	split_nav("move", "l"),
+	-- resize panes
+	split_nav("resize", "h"),
+	split_nav("resize", "j"),
+	split_nav("resize", "k"),
+	split_nav("resize", "l"),
 }
 config.set_environment_variables = {}
 
-if wezterm.target_triple == 'aarch64-apple-darwin' then
-  config.set_environment_variables.PATH = '/opt/homebrew/bin:' .. os.getenv 'PATH'
-  config.set_environment_variables.SHELL = '/opt/homebrew/bin/nu'
+if wezterm.target_triple == "aarch64-apple-darwin" then
+	config.set_environment_variables.PATH = "/opt/homebrew/bin:" .. os.getenv("PATH")
+	config.set_environment_variables.SHELL = "/opt/homebrew/bin/nu"
 end
 
-config.default_prog = {'nu'}
+config.default_prog = { "nu" }
 
 return config
