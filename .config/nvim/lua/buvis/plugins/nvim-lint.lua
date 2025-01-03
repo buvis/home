54ecb691c7988a88f6d5a7c8c6e6b20778c2a1ee
Tomáš Bouška <tomas@buvis.net>
2025-01-03 21:02:43 +0100

    Improve markdown support in nvim

--- .config/nvim/lua/buvis/plugins/nvim-lint.lua
+++ .config/nvim/lua/buvis/plugins/nvim-lint.lua
@@ -4,10 +4,17 @@ return {
   config = function()
     local lint = require("lint")
 
+    lint.linters["markdownlint-cli2"].args = {
+      "--config",
+      vim.fn.expand("~/.config/markdownlint/.markdownlint-cli2.yaml"),
+      "--",
+    }
+
     lint.linters_by_ft = {
       javascript = { "eslint_d" },
-      typescript = { "eslint_d" },
       javascriptreact = { "eslint_d" },
+      markdown = { "markdownlint-cli2" },
+      typescript = { "eslint_d" },
       typescriptreact = { "eslint_d" },
       svelte = { "eslint_d" },
     }
