302cdcb6d6ab9185864f0326b5b9f1e7cabf7ee5
Tomáš Bouška <tomas@buvis.net>
2025-04-06 12:18:05 +0200

    Update

--- .config/bash/plugins/backup.plugin.bash
+++ .config/bash/plugins/backup.plugin.bash
@@ -12,26 +12,26 @@ function backup-router () {
     mkdir -p home/bob
     mkdir -p var/unbound/etc
     mkdir -p usr/local/bin
-    scp orcus:/etc/bgpd.conf etc/bgpd.conf
-    scp orcus:/etc/dhclient.conf etc/dhclient.conf
-    scp orcus:/etc/dhcpd.conf etc/dhcpd.conf
-    scp orcus:/etc/dnscrypt-proxy-buvis.toml etc/dnscrypt-proxy-buvis.toml
-    scp orcus:/etc/dnscrypt-proxy.toml etc/dnscrypt-proxy.toml
-    scp orcus:/etc/hostname.em0 etc/hostname.em0
-    scp orcus:/etc/hostname.em1 etc/hostname.em1
-    scp orcus:/etc/hostname.em2 etc/hostname.em2
-    scp orcus:/etc/hostname.bridge0 etc/hostname.bridge0
-    scp orcus:/etc/hosts etc/hosts
-    scp orcus:/etc/mail/smtpd.conf etc/mail/smtpd.conf
-    scp orcus:/etc/newsyslog.conf etc/newsyslog.conf
-    scp orcus:/etc/pf.conf etc/pf.conf
-    scp -r orcus:/etc/protonvpn/ etc/protonvpn/
-    scp orcus:/etc/rc.conf.local etc/rc.conf.local
-    scp orcus:/etc/resolv.conf.tail etc/resolv.conf.tail
-    scp orcus:/etc/ssh/sshd_config etc/ssh/sshd_config
-    scp orcus:/etc/sysctl.conf etc/ssh/sysctl.conf
-    scp orcus:/home/bob/send-vim-tip.sh home/bob/send-vim-tip.sh
-    scp orcus:/usr/local/bin/reload_abusers.sh usr/local/bin/reload_abusers.sh
-    scp -r orcus:/var/unbound/etc/ var/unbound/
+    scp orcus.buvis.net:/etc/bgpd.conf etc/bgpd.conf
+    scp orcus.buvis.net:/etc/dhclient.conf etc/dhclient.conf
+    scp orcus.buvis.net:/etc/dhcpd.conf etc/dhcpd.conf
+    scp orcus.buvis.net:/etc/dnscrypt-proxy-buvis.toml etc/dnscrypt-proxy-buvis.toml
+    scp orcus.buvis.net:/etc/dnscrypt-proxy.toml etc/dnscrypt-proxy.toml
+    scp orcus.buvis.net:/etc/hostname.em0 etc/hostname.em0
+    scp orcus.buvis.net:/etc/hostname.em1 etc/hostname.em1
+    scp orcus.buvis.net:/etc/hostname.em2 etc/hostname.em2
+    scp orcus.buvis.net:/etc/hostname.bridge0 etc/hostname.bridge0
+    scp orcus.buvis.net:/etc/hosts etc/hosts
+    scp orcus.buvis.net:/etc/mail/smtpd.conf etc/mail/smtpd.conf
+    scp orcus.buvis.net:/etc/newsyslog.conf etc/newsyslog.conf
+    scp orcus.buvis.net:/etc/pf.conf etc/pf.conf
+    scp -r orcus.buvis.net:/etc/protonvpn/ etc/protonvpn/
+    scp orcus.buvis.net:/etc/rc.conf.local etc/rc.conf.local
+    scp orcus.buvis.net:/etc/resolv.conf.tail etc/resolv.conf.tail
+    scp orcus.buvis.net:/etc/ssh/sshd_config etc/ssh/sshd_config
+    scp orcus.buvis.net:/etc/sysctl.conf etc/ssh/sysctl.conf
+    scp orcus.buvis.net:/home/bob/send-vim-tip.sh home/bob/send-vim-tip.sh
+    scp orcus.buvis.net:/usr/local/bin/reload_abusers.sh usr/local/bin/reload_abusers.sh
+    scp -r orcus.buvis.net:/var/unbound/etc/ var/unbound/
     cd -
 }
