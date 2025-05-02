b199f7843e2393e8ab86b947cc73cd5811974f73
Tomáš Bouška <tomas@buvis.net>
2025-05-02 22:01:26 +0200

    Restore neovim config I'm most used to

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,19 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.keymap.set("i", "jj", "<Esc>", { noremap = false, desc = "Exit INSERT mode by pressing" })
+vim.keymap.set(
+  "n",
+  "Y",
+  '"+y$',
+  { noremap = true, silent = true, desc = "Yank from cursor to end of line to system clipboard" }
+)
+vim.keymap.set("n", "YY", '"+yy', { noremap = true, silent = true, desc = "Yank entire line to system clipboard" })
+vim.keymap.set("n", "<Down>", function()
+  vim.diagnostic.jump({ count = 1, float = true })
+end, { noremap = true, silent = true, desc = "Jump to next error in Diagnostic" })
+vim.keymap.set("n", "<Up>", function()
+  vim.diagnostic.jump({ count = -1, float = true })
+end, { noremap = true, silent = true, desc = "Jump to previous error in Diagnostic" })
+vim.keymap.set("v", "Y", '"+y', { noremap = true, silent = true, desc = "Yank selection to system clipboard" })
--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,3 +1,10 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Set light theme
+vim.opt.background = "light"
+-- Don't use system clipboard for yank, delete, and paste operations
+vim.opt.clipboard = ""
+-- Show file name in winbar
+vim.opt.winbar = "%=%m %f"
