6dae0b8efe98904a0151d79cb0ba5410c76b4dc7
Tomáš Bouška <tomas@buvis.net>
2025-05-02 22:19:13 +0200

    Centralize undo files and install selenized colorscheme

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -2,9 +2,21 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+local home = vim.env.HOME
+local config = home .. "/.config/nvim"
+local root = vim.env.USER == "root"
+
 -- Set light theme
 vim.opt.background = "light"
 -- Don't use system clipboard for yank, delete, and paste operations
 vim.opt.clipboard = ""
+-- Store undofiles centrally
+if root then
+  vim.opt.undofile = false -- don't create root-owned files
+else
+  vim.opt.undodir = config .. "/.undo//" -- keep undo files out of the way
+  vim.opt.undodir = vim.opt.undodir + "." -- fallback
+  vim.opt.undofile = true -- actually use undo files
+end
 -- Show file name in winbar
 vim.opt.winbar = "%=%m %f"
---
+++ .config/nvim/lua/plugins/selenized.lua
@@ -0,0 +1,11 @@
+return {
+  { "calind/selenized.nvim" },
+
+  -- Configure LazyVim to load selenized colorscheme
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "selenized",
+    },
+  },
+}
