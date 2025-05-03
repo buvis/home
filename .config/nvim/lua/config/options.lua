89e7593a2e91302d10f42276b872783cc1188035
Tomáš Bouška <tomas@buvis.net>
2025-05-03 10:02:05 +0200

    Restore whitespace characters I'm used to

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -10,6 +10,14 @@ local root = vim.env.USER == "root"
 vim.opt.background = "light"
 -- Don't use system clipboard for yank, delete, and paste operations
 vim.opt.clipboard = ""
+-- Customize whitespace characters
+vim.opt.listchars = {
+  nbsp = "⦸", -- CIRCLED REVERSE SOLIDUS (U+29B8, UTF-8: E2 A6 B8)
+  extends = "»", -- RIGHT-POINTING DOUBLE ANGLE QUOTATION MARK (U+00BB, UTF-8: C2 BB)
+  precedes = "«", -- LEFT-POINTING DOUBLE ANGLE QUOTATION MARK (U+00AB, UTF-8: C2 AB)
+  tab = "▷⋯", -- WHITE RIGHT-POINTING TRIANGLE (U+25B7, UTF-8: E2 96 B7) + MIDLINE HORIZONTAL ELLIPSIS (U+22EF, UTF-8: E2 8B AF)
+  trail = "•", -- BULLET (U+2022, UTF-8: E2 80 A2)
+}
 -- Don't create shada files for root
 if root then
   vim.opt.shada = ""
