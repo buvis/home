b234113bb04e17aeb1f4ca420636a065cf8482d4
Tomáš Bouška <tomas@buvis.net>
2025-05-06 09:58:29 +0200

    Reorganize so I can understand this 3 months from now

--- .config/nvim/lua/plugins/python.lua
+++ .config/nvim/lua/plugins/python.lua
@@ -22,4 +22,12 @@ return {
       },
     },
   },
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        python = { "ruff_organize_imports", lsp_format = "first" },
+      },
+    },
+  },
 }
