e179fcddafe43c4813cd0378697ee3e751cbfc02
Tomáš Bouška <tomas@buvis.net>
2025-05-06 14:32:15 +0200

    Fix copilot icon in completion

--- .config/nvim/lua/plugins/extend-blink.lua
+++ .config/nvim/lua/plugins/extend-blink.lua
@@ -1,9 +1,18 @@
 return {
+  { "giuxtaposition/blink-cmp-copilot", enabled = false },
   {
     "saghen/blink.cmp",
+    dependencies = { "fang2hou/blink-copilot" },
     opts = {
       keymap = {
         ["<Tab>"] = { "select_and_accept" },
+      },
+      sources = {
+        providers = {
+          copilot = {
+            module = "blink-copilot",
+          },
+        },
       },
     },
   },
