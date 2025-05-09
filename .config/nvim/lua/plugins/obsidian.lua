2702e7ca1db21fd0a4e87708f06e1ec70f027561
Tomáš Bouška <tomas@buvis.net>
2025-05-09 20:18:22 +0200

    Install obsidian-like functionality

---
+++ .config/nvim/lua/plugins/obsidian.lua
@@ -0,0 +1,19 @@
+return {
+  "obsidian-nvim/obsidian.nvim",
+  version = "*",
+  lazy = true,
+  ft = "markdown",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+  },
+  ---@module 'obsidian'
+  ---@type obsidian.config.ClientOpts
+  opts = {
+    workspaces = {
+      {
+        name = "bim",
+        path = "~/bim/",
+      },
+    },
+  },
+}
