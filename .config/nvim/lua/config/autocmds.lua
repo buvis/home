5c01b5534cfac7e42f76ab6d57017627178679e1
Tomáš Bouška <tomas@buvis.net>
2025-05-20 06:51:49 +0200

    Open plugins repository using gx

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -81,3 +81,51 @@ vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
   end,
   desc = "Disable Copilot in private folder",
 })
+
+--[[ Open plugin repos with gx ]]
+vim.api.nvim_create_autocmd("BufReadPost", {
+  group = vim.api.nvim_create_augroup("GxWithPlugins", { clear = true }),
+  callback = function()
+    local cwd = vim.fn.getcwd()
+    local config_dir = vim.fn.stdpath("config")
+
+    if cwd == config_dir or cwd:sub(1, #config_dir + 1) == config_dir .. "/" then
+      vim.keymap.set("n", "gx", function()
+        local file = vim.fn.expand("<cfile>") --[[@as string]]
+
+        -- First try the default behavior
+        -- see https://github.com/neovim/neovim/blob/b0f9228179bf781eec76d1aaf346b56a7e64cd5d/runtime/lua/vim/_defaults.lua#L101
+        -- for recent changes in `vim.ui.open`
+        local cmd, err = vim.ui.open(file)
+        local rv = cmd and cmd:wait(1000) or nil
+
+        if cmd and rv and rv.code ~= 0 then
+          err = ("vim.ui.open: command %s (%d): %s"):format(
+            (rv.code == 124 and "timeout" or "failed"),
+            rv.code,
+            vim.inspect(cmd.cmd)
+          )
+        end
+
+        if not err then
+          return
+        end
+
+        -- Consider anything that looks like string/string a GitHub link.
+        local link = file:match("%w[%w%-]+/[%w%-%._]+")
+
+        if link then
+          vim.ui.open("https://www.github.com/" .. link)
+          err = nil
+        end
+
+        -- Else show the error
+
+        if err then
+          vim.notify(err, vim.log.levels.ERROR)
+        end
+      end, { desc = "Open filepath or URI under cursor" })
+    end
+  end,
+  desc = "Make `gx` open repos in default browser",
+})
