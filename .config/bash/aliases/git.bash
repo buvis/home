aeb087155b4571225ca05a4d2fbd977811c73946
Tomáš Bouška <tomas@buvis.net>
2025-05-21 19:48:29 +0200

    Add alias for lazygit

--- .config/bash/aliases/git.bash
+++ .config/bash/aliases/git.bash
@@ -1,4 +1,5 @@
-if ! python3 -m gita &> /dev/null; then
-    pip3 install -U gita
+if ! python3 -m gita &>/dev/null; then
+  pip3 install -U gita
 fi
 alias gita="python3 -m gita"
+alias gg="lazygit"
