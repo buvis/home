bb2473d8bf36d4eb2e156c6e38382032b99615d0
Tomáš Bouška <tomas@buvis.net>
2026-01-04 11:36:48 +0100

    fix: remove unused aliases

--- .config/bash/aliases/development.bash
+++ .config/bash/aliases/development.bash
@@ -1,3 +1,1 @@
-alias update-doogat='gita shell doogat make update'
-alias build-doogat='gita shell pingl make build; gita shell doo make build'
 alias start-remote-dev='tmux attach -t remote_dev || tmux new -s remote_dev'
