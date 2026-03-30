4f27c05e5d219586edf5fce5cfc3ad51b0888145
Test <test@test.com>
2026-03-30 19:57:48 +0200

    chore: remove unused aliases

--- .config/bash/aliases/directory-bookmarks.bash
+++
@@ -1,3 +0,0 @@
-# https://stackoverflow.com/questions/7374534/directory-bookmarking-for-bash#comment125072031_30864873
-alias cdb='echo ${PWD/#$HOME/\~} >> $DOTFILES_ROOT/.fzfcd; sort -u -o $DOTFILES_ROOT/.fzfcd < $DOTFILES_ROOT/.fzfcd'
-alias cdf='target="$(fzf < ~/.fzfcd)" && cd "${target/#\~/$HOME}"'
--- .config/bash/aliases/zettelkasten.bash
+++
@@ -1,2 +0,0 @@
-alias z='vim ~/z/reference/notes/zettelkasten/index.md'
-alias get-last-photo-seq='zseq -m -p /Volumes/photography/photography/src/2025'
